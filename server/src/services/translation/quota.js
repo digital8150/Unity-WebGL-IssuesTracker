8e788c46ac8a7fd839b2b980711c3e7845d34ea3
Digital <73296205+digital8150@users.noreply.github.com>
2026-08-21 19:47:54 +0900

    fix: normalize Pacific midnight on Node 20

--- server/src/services/translation/quota.js
+++ server/src/services/translation/quota.js
@@ -2,7 +2,7 @@ function localParts(date) {
   const parts = new Intl.DateTimeFormat('en-US', {
     timeZone: 'America/Los_Angeles',
     year: 'numeric', month: '2-digit', day: '2-digit',
-    hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false,
+    hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h23',
   }).formatToParts(date);
   return Object.fromEntries(parts.filter((part) => part.type !== 'literal').map((part) => [part.type, Number(part.value)]));
 }
